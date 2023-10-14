75a42d00daa2ca6c226ad7d6d73d082e42989e1e
mili torres <militorres018@gmail.com>
2023-10-14 16:16:42 -0300

    first push, server w/ express

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Y ahora como conecto esto a mi proyecto con react?")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  })
